eea3894c028eb998b735afd468da8740927de54b
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-10-21 15:11:38 +0300

    feat: add header markup

---
+++ src/component/Hero.tsx
@@ -0,0 +1,8 @@
+const Hero = () => {
+  return (
+    <section className="intro container h-96  px-28 pt-44 text-white">
+      <h1 className="hero-title mb-16 font-namu ">Здраве життя</h1>
+    </section>
+  );
+};
+export default Hero;
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,12 @@
+import Header from '@/component/Header';
+import Hero from '@/component/Hero';
+
 const HomePage = () => {
   return (
-    <div className="intro flex h-screen w-full flex-col items-center justify-center font-namu text-5xl text-white">
-      <div className="title absolute left-[4rem] top-1/2">
-        <h1>Здраве життя</h1>
-      </div>
-    </div>
+    <>
+      <Header />
+      <Hero />
+    </>
   );
 };
 
