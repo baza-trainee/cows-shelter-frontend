1bf3daffabbfd52f934ff0bbbd4daebec8aea44d
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-10-21 19:48:17 +0300

    feat: add hero markup

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,15 +8,8 @@ const HomePage = () => {
     <>
       <Header />
       <Hero />
-      <div className="intro flex h-screen w-full flex-col items-center justify-center font-namu text-5xl text-white">
-        <div className="title absolute left-[4rem] top-1/2">
-          <h1>Здраве життя</h1>
-        </div>
-      </div>
-      <div>
-        <Footer />
-        <UnderFooter />
-      </div>
+      <Footer />
+      <UnderFooter />
     </>
   );
 };
