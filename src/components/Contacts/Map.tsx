935ef72a293f9de4b87d0f02bd9f2d287ed221df
Sofia Ruban <sophiaruban249324@gmail.com>
2023-10-26 21:25:17 +0300

    feat: Add map component

--- src/components/Contacts/Map.tsx
+++ src/components/Contacts/Map.tsx
@@ -1,21 +1,26 @@
-//import { Icon } from 'leaflet';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
-//import icon from '@/assets/icons/icon_marker.png'
+import icon from '@/assets/icons/icon_marker.png'
 
 const Map = () => {
+    const customIcon = new Icon({
+      iconUrl: icon,
+      iconSize: [40, 60]
+    });
   return (
-    <div>
-      <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <div className="h-full w-full">
+      <MapContainer
+        center={[48.3302805, 28.1315992]}
+        zoom={13}
+        scrollWheelZoom={false}
+        style={{ height: '560px', width: '1440px' }}
+      >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[51.505, -0.09]}>
-          <Popup>
-            A pretty CSS3 popup. <br /> Easily customizable.
-          </Popup>
-        </Marker>
+        <Marker position={[48.3302805, 28.1315992]} icon={customIcon} />
       </MapContainer>
     </div>
   );
