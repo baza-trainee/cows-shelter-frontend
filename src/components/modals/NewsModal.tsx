89b2180fbb3dd7b244194076192af541a505fe8c
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-07 15:33:48 +0100

    fix(NewsModal): News data type is unreachable

--- src/components/modals/NewsModal.tsx
+++ src/components/modals/NewsModal.tsx
@@ -3,6 +3,7 @@ import { useAppDispatch, useAppSelector } from '@/store/hook';
 import { closeModal } from '@/store/slices/modalSlice';
 import CloseIcon from '../icons/CloseIconMenu';
 import { Dispatch, SetStateAction } from 'react';
+import { NewsData } from '@/types';
 
 type NewsModalProps = {
   isOpen: boolean;
@@ -11,7 +12,7 @@ type NewsModalProps = {
 
 const NewsModal = ({ isOpen, setShowModal }: NewsModalProps) => {
   const dispatch = useAppDispatch();
-  const news = useAppSelector((state) => state.modals.data);
+  const news = useAppSelector((state) => state.modals.data) as NewsData;
 
   const handleCloseModal = () => {
     setShowModal(false);
@@ -32,21 +33,21 @@ const NewsModal = ({ isOpen, setShowModal }: NewsModalProps) => {
         } max-h-[582px] max-w-full overflow-auto overflow-y-auto bg-white  p-6 px-5 pb-6  pt-9  md:max-h-[832px] md:w-[672px] md:px-10 lg:w-[1136px] lg:py-16 `}
       >
         <h2 className="text-lg font-semibold lg:divide-y-4 lg:text-2xl lg:font-bold">
-          {news?.title}
+          {news.title}
         </h2>
         <hr className=" my-2 h-px border-t-0 bg-slate-300 opacity-0 md:opacity-100" />
         <div className="py-4">
           <div className="flex  items-center">
             <img src={iconCalendar} alt="Image" className="mr-3" />
-            <p className="text-sm font-normal">Опубліковано {news?.date}</p>
+            <p className="text-sm font-normal">Опубліковано {news.date}</p>
           </div>
         </div>
         <div className="sm: w-full text-justify sm:text-left  lg:columns-2 lg:px-16">
           <img
             className="mx-auto mb-4 h-52  object-contain md:h-52 md:w-[582px] md:object-cover lg:h-[278px] lg:w-[488px] lg:object-cover  "
-            src={news?.url}
+            src={news.url}
           />
-          <p>{news?.text}</p>
+          <p>{news.text}</p>
         </div>
         <button
           type="button"
