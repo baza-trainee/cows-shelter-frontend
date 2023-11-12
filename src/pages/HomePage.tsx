98873035fb3fa8ff47ab6ec3484984771887dca3
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-11-12 22:00:23 +0200

    feat(partners): add mobile partners modal layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,32 +1,32 @@
-// import Contacts from '@/components/Contacts/Contacts';
-// import Header from '@/components/Header/Header';
-// import Hero from '@/components/Hero';
-// import Footer from '@/components/Footer/Footer';
-// import UnderFooter from '@/components/Footer/UnderFooter';
+import Contacts from '@/components/Contacts/Contacts';
+import Header from '@/components/Header/Header';
+import Hero from '@/components/Hero';
+import Footer from '@/components/Footer/Footer';
+import UnderFooter from '@/components/Footer/UnderFooter';
 import Layout from '@/components/Layout';
 import Excursions from '@/components/Excursions/Excursions';
 import Gallery from '@/components/Gallery/Gallery';
 import Partners from '@/components/Partners';
-// import News from '@/components/News/News';
-// import FAQ from '@/components/FAQ';
-// import Support from '@/components/Support/Support';
-// import About from '@/components/About/About';
+import News from '@/components/News/News';
+import FAQ from '@/components/FAQ';
+import Support from '@/components/Support/Support';
+import About from '@/components/About/About';
 
 const HomePage = () => {
   return (
     <Layout>
-      {/* <Header /> */}
-      {/* <Hero /> */}
-      {/* <About /> */}
+      <Header />
+      <Hero />
+      <About />
       <Excursions />
       <Gallery />
-      {/* <News /> */}
+      <News />
       <Partners />
-      {/* <Support />
+      <Support />
       <FAQ />
       <Contacts />
       <Footer />
-      <UnderFooter /> */}
+      <UnderFooter />
     </Layout>
   );
 };
