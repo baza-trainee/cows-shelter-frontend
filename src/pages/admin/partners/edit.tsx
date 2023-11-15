5c9902ff26a2fa1bf6eb960cd719103cc44f70ff
Oleh Buhaichuk <oleh.buhaichuk@gmail.com>
2023-11-16 00:40:17 +0200

    feat(admin-partners/edit): add layout to partners/edit page

--- src/pages/admin/partners/edit.tsx
+++ src/pages/admin/partners/edit.tsx
@@ -48,84 +48,95 @@ const EditPartner = () => {
   const onSubmit: SubmitHandler<PartnersFormInput> = () => {};
 
   return (
-    <div className="flex min-h-screen w-full flex-col items-start justify-center gap-4 pb-[134px] pl-[48px] pr-[142px] ">
-      <div className="mb-[12px] mt-[48px]">
-        <h1 className="text-3xl font-bold">Редагування Партнера</h1>
-      </div>
-      <div className="flex w-full">
-        <form
-          onSubmit={handleSubmit(onSubmit)}
-          className="flex flex-1 flex-col gap-4"
-        >
-          <div className="flex gap-2">
-            <FileInput
-              name="image"
-              control={control}
-              accept="image/*"
-              placeholder={'Оберіть файл'}
-              title="Змінити логотип Партнера:"
-            />
-            <Controller
-              name="title"
-              rules={{ required: 'Введіть назву' }}
-              control={control}
-              render={({ field }) => (
-                <TextInput
-                  {...field}
-                  errorText={errors.title?.message}
-                  placeholder="Введіть назву Партнера"
-                  title="Змінити назву Партнера:"
-                />
-              )}
-            />
+    <div className="flex flex-col gap-4 px-[108px] pt-[60px] ">
+      <div className="flex  ">
+        <div>
+          <div>
+            <h1 className="text-3xl font-bold">Редагування Партнера</h1>
+          </div>
 
-            <section className="flex flex-col items-center justify-center gap-4">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-1 flex-col gap-10"
+          >
+            <div className="flex flex-col gap-6">
+              <FileInput
+                name="image"
+                control={control}
+                accept="image/*"
+                placeholder={'Оберіть файл'}
+                title="Змінити логотип Партнера:"
+                className="w-full "
+              />
               <Controller
-                name="href"
-                rules={{ required: 'Введіть URL' }}
+                name="title"
+                rules={{ required: 'Введіть назву' }}
                 control={control}
                 render={({ field }) => (
                   <TextInput
                     {...field}
-                    errorText={errors.href?.message}
-                    placeholder="Введіть посилання на сторінку Партнера"
-                    title="Змінити посилання на сторінку Партнера:"
+                    errorText={errors.title?.message}
+                    placeholder="Введіть назву Партнера"
+                    title="Змінити назву Партнера:"
                   />
                 )}
               />
-            </section>
 
-            <section className="flex flex-col items-center justify-center gap-4 px-8">
-              <div className="mt-[5vh] flex w-full flex-col items-center justify-center gap-8 ">
-                <div className="relative text-left">
-                  <img
-                    src={image ? image : '/placeholder-image.jpeg'}
-                    alt={currentValues.title}
-                    className="h-[240px] w-[320px] rounded-md object-cover"
-                  />
-                  <h2
-                    className={`absolute bottom-4 left-2 text-xl font-bold ${
-                      !image ? 'text-gray-400' : 'text-white'
-                    } `}
-                  >
-                    {currentValues.title ? currentValues.title : 'Заголовок...'}
-                  </h2>
-                </div>
-              </div>
-            </section>
-          </div>
-          <div className="flex gap-4">
-            <button className=" w-[13.5rem] rounded-md bg-gray-200 px-6 py-2 transition-all hover:bg-lemon">
-              Розмістити
-            </button>
-
-            <Link to="/admin">
-              <button className="w-[13.5rem] rounded-md border-2 border-lightgrey bg-white px-6 py-2 transition-all hover:bg-red-300">
-                Скасувати
+              <section className="flex flex-col items-center justify-center gap-4">
+                <Controller
+                  name="href"
+                  rules={{ required: 'Введіть URL' }}
+                  control={control}
+                  render={({ field }) => (
+                    <TextInput
+                      {...field}
+                      errorText={errors.href?.message}
+                      placeholder="Введіть посилання на сторінку Партнера"
+                      title="Змінити посилання на сторінку Партнера:"
+                    />
+                  )}
+                />
+              </section>
+            </div>
+            <div className="flex gap-4">
+              <button className=" w-[13.5rem] rounded-md bg-gray-200 px-6 py-2 transition-all hover:bg-lemon">
+                Розмістити
               </button>
-            </Link>
+
+              <Link to="/admin">
+                <button className="w-[13.5rem] rounded-md border-2 border-lightgrey bg-white px-6 py-2 transition-all hover:bg-red-300">
+                  Скасувати
+                </button>
+              </Link>
+            </div>
+          </form>
+        </div>
+
+        <section className="flex flex-col items-center  gap-4 px-8">
+          <div className="flex  flex-col items-center  gap-8 ">
+            <div className="text-left">
+              <img
+                src={image ? image : '/placeholder-image.jpeg'}
+                alt={currentValues.title}
+                width={205}
+                height={205}
+                className="rounded-md"
+              />
+              <h2
+                className={` bottom-4 left-2 text-xl font-bold
+                `}
+              >
+                {currentValues.title}
+              </h2>
+              <h2
+                className={` bottom-4 left-2 text-xl font-bold
+                `}
+              >
+                {currentValues.href}
+              </h2>
+            </div>
           </div>
-        </form>
+        </section>
       </div>
     </div>
   );
