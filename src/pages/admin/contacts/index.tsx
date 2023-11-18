bec98c1f1fe25632e151ff90073ea29a923335d9
Sofia Ruban <sophiaruban249324@gmail.com>
2023-11-18 11:53:02 +0200

    fix: Fix mobile version

--- src/pages/admin/contacts/index.tsx
+++ src/pages/admin/contacts/index.tsx
@@ -13,43 +13,42 @@ const Contacts = () => {
   // const [showConfirm, setShowConfirm] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="relative flex min-h-screen flex-col items-center justify-center p-8">
-      <div className="mb-4 flex w-2/3 items-center justify-between border-b border-black p-2">
-        <h2 className="text-center text-lg font-bold text-darkgray">
-          {contacts.email}
-        </h2>
-        <div className="flex gap-2">
+    <div className="relative flex min-h-screen flex-col items-start pt-10">
+      <h3 className="mb-8 ml-8 text-[32px] font-semibold">Контакти</h3>
+      <div className="mb-4 flex max-w-[21.5rem] flex-col items-center justify-between border border-black px-[90px] ml-8 pt-8">
+        <h2 className="pb-2 text-center text-base">Ваша електронна адреса:</h2>
+        <p className="pb-8 text-lg font-bold text-darkgray">{contacts.email}</p>
+        <div className="flex bg-lightgrey">
           <button
             onClick={() => {
               setData(contacts.email), setIsModalOpen(true);
             }}
-            className="text-xl text-black hover:text-accent"
+            className="lightgrey border border-r-darkgray border-t-darkgray px-[4.65rem]  py-2 text-xl text-black hover:text-accent"
           >
             <BsFillPencilFill />
           </button>
           <button
-            className="text-xl text-black hover:text-red-500"
+            className="border border-t-darkgray px-[4.65rem] py-2 text-xl text-black hover:text-red-500"
             // onClick={() => setShowConfirm(true)}
           >
             <BsFillTrash3Fill />
           </button>
         </div>
       </div>
-      <div className="flex w-2/3 items-center justify-between border-b border-black p-2">
-        <h2 className="text-center text-lg font-bold text-darkgray">
-          {contacts.phone}
-        </h2>
-        <div className="flex gap-2">
+      <div className="flex max-w-[21.5rem] flex-col items-center justify-between border border-black px-[90px] ml-8  pt-8">
+        <h2 className="pb-2 text-center text-base">Ваш номер телефону:</h2>
+        <p className="pb-8 text-lg font-bold text-darkgray">{contacts.phone}</p>
+        <div className="flex bg-lightgrey">
           <button
             onClick={() => {
               setData(contacts.phone), setIsModalOpen(true);
             }}
-            className="text-xl text-black hover:text-accent"
+            className="border border-r-darkgray border-t-darkgray  px-[4.65rem] py-2 text-xl text-black hover:text-accent "
           >
             <BsFillPencilFill />
           </button>
           <button
-            className="text-xl text-black hover:text-red-500"
+            className="border border-t-darkgray px-[4.65rem] py-2 text-xl text-black hover:text-red-500"
             // onClick={() => setShowConfirm(true)}
           >
             <BsFillTrash3Fill />
