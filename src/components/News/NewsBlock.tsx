8e1c8f27c8b71abf7cdf4a99400fa2ab145dbcca
Iakiv Pekarskyi <pekarskiy88@gmail.com>
2023-11-22 20:24:17 +0100

    feat(newsblock): pagination not available

--- src/components/News/NewsBlock.tsx
+++ src/components/News/NewsBlock.tsx
@@ -4,11 +4,12 @@ import { newsItems } from '@/data/newsItems';
 import { NewsData } from '@/types';
 import { useWidth } from '@/hooks/useWidth';
 import { useState, useEffect } from 'react';
+import LittleArrow from '../icons/LittleArrow';
 
 const NewsBlock = () => {
   const dispatch = useAppDispatch();
   const screenWidth = useWidth();
-  const [itemsPerPage, setItemsPerPage] = useState(3); // Update as needed based on screen size
+  const [itemsPerPage, setItemsPerPage] = useState(1);
   const [currentPage, setCurrentPage] = useState(1);
 
   const openNewsModal = (item: NewsData) => {
@@ -19,8 +20,8 @@ const NewsBlock = () => {
     if (screenWidth > 1280) {
       setItemsPerPage(5);
     } else if (screenWidth <= 1280 && screenWidth > 768) {
-      setItemsPerPage(5);
-      setItemsPerPage(1);
+      setItemsPerPage(3);
+      setItemsPerPage(3);
     }
   }, [screenWidth]);
 
@@ -34,8 +35,8 @@ const NewsBlock = () => {
         {visibleItems.map((news, index) => (
           <li
             key={news.id}
-            className={`group relative cursor-pointer ${
-              index === 0 ? 'h-586 w-46 row-span-2' : ''
+            className={`group relative cursor-pointer${
+              index === 0 ? 'h-300 w-46 row-span-5' : ''
             } ${index >= 3 ? 'hidden lg:block' : ''}`}
           >
             <img
@@ -47,7 +48,9 @@ const NewsBlock = () => {
             <div className="via-opacity-30 absolute inset-0 z-30 flex cursor-pointer flex-col bg-gradient-to-b from-transparent  to-black/70  sm:to-black/10   ">
               <div className="flex h-full flex-col justify-end text-white">
                 <div className="translate-y-14 space-y-3 p-4 duration-300 ease-in-out group-hover:translate-y-0">
-                  <h2 className="text-2xl font-normal">{news.title}</h2>
+                  <h2 className="text-lg font-normal lg:text-2xl">
+                    {news.title}
+                  </h2>
                   <div className="text-sm opacity-0 group-hover:opacity-100">
                     {news.description}
                   </div>
@@ -56,10 +59,11 @@ const NewsBlock = () => {
               <div>
                 <button
                   onClick={() => openNewsModal(news)}
-                  className="relative mb-6 ml-6 mt-5 border-2 border-transparent py-1 text-white group-hover:border-yellow-500"
+                  className="relative mb-6 ml-6 mt-5 border py-1 text-white sm:group-hover:border-yellow-500 md:border-none lg:border-transparent"
                 >
-                  <div className="flex items-center">
-                    <p className="px-4">Показати більше</p>
+                  <div className="color-white flex items-center px-4 py-1">
+                    <p className="pr-2">Показати більше</p>
+                    <LittleArrow />
                   </div>
                 </button>
               </div>
