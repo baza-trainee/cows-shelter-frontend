a3acc7e80c5486c478f937729286e18df966c781
mariastetciuk <114301391+mariastetciuk@users.noreply.github.com>
2023-11-29 22:44:46 +0200

    feat: add ProtectedRoute

--- src/pages/admin/ProtectedRoute.tsx
+++ src/pages/admin/ProtectedRoute.tsx
@@ -1,15 +1,19 @@
 import { useEffect } from 'react';
 import { Outlet, useNavigate } from 'react-router-dom';
+import { token } from './login/fetchin/fetchin';
 
 const ProtectedRoute = () => {
   const navigate = useNavigate();
-  const isAuthenticated = false;
+  const user = localStorage.getItem('user');
+  const { access_token } = JSON.parse(user as string) ?? {};
 
   useEffect(() => {
-    if (!isAuthenticated) {
+    if (!access_token) {
       navigate('/');
+    } else {
+      token.set(access_token);
     }
-  }, [isAuthenticated, navigate]);
+  }, [navigate, access_token]);
 
   return <Outlet />;
 };
