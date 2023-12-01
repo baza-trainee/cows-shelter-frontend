d0b60c15b6b22870c280e21f3981aff215a3289f
cold <kholodihor@gmail.com>
2023-12-01 13:28:43 +0200

    feat(partners): finalized partners section

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
+import { useWidth } from '@/hooks/useWidth';
 import { useInView } from 'react-intersection-observer';
 import { setActiveLink } from '@/store/slices/observationSlice';
 import { useAppDispatch, useAppSelector } from '@/store/hook';
@@ -7,32 +8,60 @@ import { useAppDispatch, useAppSelector } from '@/store/hook';
 import Slider from '@/components/Slider';
 import PartnersModal from './modals/PartnersModal';
 import { openModal } from '@/store/slices/modalSlice';
-import { usePaginatedData } from '@/hooks/usePaginatedData';
-import { Partner, fetchPartners } from '@/store/slices/partnersSlice';
 import Loader from './admin/Loader';
+import {
+  Partner,
+  fetchPartnersWithPagination
+} from '@/store/slices/partnersSlice';
 
 const Partners = () => {
   const { t } = useTranslation();
-  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
+  const screenWidth = useWidth();
   const dispatch = useAppDispatch();
-  const partners = useAppSelector((state) => state.partners.partners);
-  const isLoading = useAppSelector((state) => state.partners.loading);
 
+  const [pagesLength, setPagesLength] = useState(0);
+  const [itemsPerPage, setItemsPerPage] = useState(4);
+  const [currentPage, setCurrentPage] = useState(1);
+  const isLoading = useAppSelector((state) => state.partners.loading);
   const type = useAppSelector((state) => state.modals.type);
   const isModalOpen = useAppSelector((state) => state.modals.isModalOpen);
+  const { partners, totalLength } = useAppSelector(
+    (state) => state.partners.paginatedData
+  );
+
   const { ref, inView } = useInView({
     threshold: 0.5
   });
 
   useEffect(() => {
-    dispatch(fetchPartners())
+    if (screenWidth > 1280) {
+      setItemsPerPage(4);
+    }
+    if (screenWidth >= 768 && screenWidth < 1280) {
+      setItemsPerPage(3);
+    }
+    if (screenWidth > 320 && screenWidth < 768) {
+      setItemsPerPage(totalLength);
+    }
+  }, [screenWidth, totalLength]);
+
+  useEffect(() => {
+    const pagesNumber = totalLength / itemsPerPage;
+    setPagesLength(pagesNumber < 5 ? pagesNumber : 5);
+  }, [totalLength, itemsPerPage]);
+
+  useEffect(() => {
+    dispatch(
+      fetchPartnersWithPagination({ page: currentPage, limit: itemsPerPage })
+    )
       .unwrap()
       .then(() => {
         return [];
       })
       .catch((error) => alert(error));
-  }, [dispatch]);
+  }, [dispatch, currentPage, itemsPerPage]);
+
+  console.log(partners);
 
   const openPartnersModal = () => {
     dispatch(openModal({ data: {}, type: 'partners' }));
@@ -53,71 +82,12 @@ const Partners = () => {
   }, [isModalOpen]);
 
   useEffect(() => {
-    const handleChangedSize = () => {
-      setWindowWidth(window.innerWidth);
-    };
-    window.addEventListener('resize', handleChangedSize);
-    return () => {
-      window.removeEventListener('resize', handleChangedSize);
-    };
-  }, []);
-
-  useEffect(() => {
     if (inView) {
       dispatch(setActiveLink('#partners'));
     } else {
       dispatch(setActiveLink(''));
     }
   }, [inView, dispatch]);
-
-  const [start, setStart] = useState(0);
-  const [finish, setFinish] = useState(3);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [itemsPerPage, setItemsPerPage] = useState(3);
-  const pagesLength = partners.length / itemsPerPage + 1;
-
-  const data = usePaginatedData(partners, start, finish);
-
-  useEffect(() => {
-    if (windowWidth >= 1280) {
-      setItemsPerPage(4);
-      if (currentPage === 1) {
-        setStart(0);
-        setFinish(4);
-      }
-      if (currentPage === 2) {
-        setStart(1);
-        setFinish(5);
-      }
-      if (currentPage === 3) {
-        setStart(0);
-        setFinish(4);
-      }
-    }
-    if (windowWidth >= 768 && windowWidth < 1280) {
-      setItemsPerPage(3);
-      if (currentPage === 1) {
-        setStart(0);
-        setFinish(3);
-      }
-      if (currentPage === 2) {
-        setStart(1);
-        setFinish(4);
-      }
-      if (currentPage === 3) {
-        setStart(2);
-        setFinish(5);
-      }
-    }
-  }, [windowWidth, currentPage]);
-
-  useEffect(() => {
-    if (isModalOpen === true) {
-      document.body.style.overflow = 'hidden';
-    } else {
-      document.body.style.overflow = 'auto';
-    }
-  }, [isModalOpen]);
 
   if (isLoading) return <Loader />;
 
@@ -177,24 +147,24 @@ const Partners = () => {
             pagesLength={pagesLength}
           >
             <ul className="mb-5  flex gap-6 lg:mt-20">
-              {data && Array.isArray(data) ? (
-                data.map((item: Partner) => (
-                  <li key={item.id} className="flex justify-around">
+              {partners && Array.isArray(partners) ? (
+                partners.map((partner: Partner) => (
+                  <li key={partner.id} className="flex justify-around">
                     <a
-                      href={item.link}
+                      href={partner.link}
                       target="_blank"
                       rel="noopener noreferrer"
                       className="partner-scale block w-full transform border-solid border-darkyellow transition-all duration-300 hover:border-b"
                     >
                       <img
                         className="m-auto mb-6 scale-100 transform md:h-[208px] md:w-[208px] xl:h-[245px] xl:w-[245px]"
-                        src={item.logo}
-                        alt={item.name}
+                        src={partner.logo}
+                        alt={partner.name}
                         width={208}
                         height={208}
                       />
                       <p className=":w-[282px] mb-5 w-[208px] text-center text-[1rem] leading-relaxed md:text-[20px] lg:mb-[4.125rem] lg:text-[22px]">
-                        {item.name}
+                        {partner.name}
                       </p>
                     </a>
                   </li>
